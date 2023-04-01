4c8bfc578749b57041c6814a2b3a1bd7f3bc43b6
Tomas Nunez <tomasfnunez@gmail.com>
2023-04-01 19:06:28 -0300

    Primera preentrega

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ItemListContainer = (props) =>{
+    return(
+        <div>
+            <h2>ItemListContainer</h2>
+            <hr/>
+            <h3>{props.greeting}</h3>
+        </div>
+    )
+}
